9800c51d76207426df26c26eb6b8306c67de4d02
StianKristoffer <49064756+StianKristoffer@users.noreply.github.com>
2019-03-29 13:40:03 +0100

    Fikset bugs - nå virker det

--- Iris/oppgave_1a.m
+++ Iris/oppgave_1a.m
@@ -1,6 +1,6 @@
-x1all = load('class_1','-ascii');
-x2all = load('class_2','-ascii');
-x3all = load('class_3','-ascii');
+x1all = load('class_1','-ascii'); % setosa
+x2all = load('class_2','-ascii'); % versicolor
+x3all = load('class_3','-ascii'); % virginica
 
 N_train = 30;
 C = 3; % Number of classes
@@ -17,14 +17,18 @@ x1test = x1all(N_train+1:50, :);
 x2test = x2all(N_train+1:50, :);
 x3test = x3all(N_train+1:50, :);
 
+x_all_test = [x1test; x2test; x3test]';
+t_all_test = [repmat([1 0 0],(50-N_train),1); repmat([0 1 0],(50-N_train),1); repmat([0 0 1],(50-N_train),1)]';
+
 % g_i(x) = w_i'*x + w_i0,   i = {1, ...C}
 % g = Wx + w_0
 % g and w_0 are vectors of dimention C(numer of classes=3), and W is a CxD matrix
 % C: number of classes (3)
 % D: Number of features (4)
 
-W   = (randi(7, C, D)-4)/3; % 3x4 matrise
-w_0 = (randi(7, C, 1)-4)/3; %3x1 vektor
+W   = zeros(C, D); % 3x4 matrise
+w_0 = zeros(C, 1); %3x1 vektor
+
 W = [W, w_0];
 x_vec = [x_all_train', ones(90,1)]';
 
@@ -38,34 +42,48 @@ MSE = 0;
 for k=1 % C*N_train % k = 1:90
     MSE = MSE + 1/2 * (g(:,k)-t(:,k))'*(g(:,k)-t(:,k));
 end
+MSE;
 
 
-
-wrongly_classified = sum(sum(((g==(max(g))) - t) ==1))
-
-alpha = 0.1;
-for iteration = 1:10000
+alpha = 0.005;
+for iteration = 1:5000
     
     z = W * x_vec;
     g = 1./(1+exp(-z)); % Squashing function
     
     grad_W_MSE = 0;
-    for k=1 %C*N_train % k = 1:90
+    for k=1:C*N_train % k = 1:90
         grad_W_MSE = grad_W_MSE + ...
              ( (g(:,k)-t(:,k)) .* g(:,k) .* (1-g(:,k)) )...
             * x_vec(:,k)';
     end
-    W = W - alpha * grad_W_MSE;
     
+    W = W - alpha * grad_W_MSE;
+    %%{
+    if (mod(iteration,100)==0 && 0)
+        MSE = 0;
+        for k=1:C*N_train % k = 1:90
+            MSE = MSE + 1/2 * (g(:,k)-t(:,k))'*(g(:,k)-t(:,k));
+        end
+        MSE
+    end
+    %{
+    %}
 end
 
 
+fprintf('After the classifier training, %i of the %i flowers in the training set are classified correctly.\n', ...
+    C*N_train-sum(sum(((g==(max(g))) - t) ==1)),...
+    C*N_train ...
+);
 
+% Tester på testsettet:
+x_vec = [x_all_test', ones(60,1)]';
+z = W * x_vec;
+g = 1./(1+exp(-z)); % Squashing function
 
-wrongly_classified = sum(sum(((g==(max(g))) - t) ==1))
-
-
-
-
-
+fprintf('After the classifier training, %i of the %i flowers in the test set are classified correctly.\n\n', ...
+    C*(50-N_train)-sum(sum(((g==(max(g))) - t_all_test) ==1)),...
+    C*(50-N_train) ...
+);
 
